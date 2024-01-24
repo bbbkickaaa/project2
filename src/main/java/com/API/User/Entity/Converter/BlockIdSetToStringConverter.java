86c471beb1376de3b0a004253bad5eecb5f92f70
박성민 <bbbkickaaa@gmail.com>
2024-01-24 21:06:25 +0900

    chore 1.2.2.1

---
+++ src/main/java/com/API/User/Entity/Converter/BlockIdSetToStringConverter.java
@@ -0,0 +1,45 @@
+package com.API.User.Entity.Converter;
+
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
+import java.util.stream.Collectors;
+import jakarta.persistence.*;
+
+@Converter
+public class BlockIdSetToStringConverter implements AttributeConverter<Set<Integer>, String> {
+	
+    @Override
+    public String convertToDatabaseColumn(Set<Integer> blockIds) {
+    	
+    	String column;
+    	
+        if (blockIds == null || blockIds.isEmpty()) {
+        	column="";
+        }
+        else {
+        	column = blockIds.stream()
+        			.map(String::valueOf)
+        			.collect(Collectors.joining(","));
+        	}
+        
+        return column;
+    }
+
+    @Override
+    public Set<Integer> convertToEntityAttribute(String dbData) {
+    	
+    	Set<Integer> blockId;
+    	
+        if (dbData == null || dbData.trim().isEmpty()) {
+            blockId =  new HashSet<>();
+        }
+        else {
+        	blockId = Arrays.stream(dbData.split(","))
+                     .map(Integer::valueOf)
+                     .collect(Collectors.toSet());
+        }
+        return blockId;
+    }
+    
+}
